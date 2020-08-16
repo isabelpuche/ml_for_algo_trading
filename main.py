32f0e0d7fcacaed97c808236bf4284f59099f828
isabelpuche <isabelpuche@gmail.com>
2020-08-16 18:42:42 +0200

    Random Forest Trading Bot

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import numpy as np
 from strategies.run_zipline import run_strategy
 
 def main():
-    print("*** KSchool Master of Data Science ed.19 Capstone Project: Machine Learning for IBEX35 stocks Algorithmic Trading ***")
+    print("*** KSchool Master of Data Science ed.19 Capstone Project: Machine Learning for NYSE stocks Algorithmic Trading ***")
+    print("*** Plug-in Random Forest Implementation into Trading Bot***")
     perf = run_strategy('buy_and_hold')
     perf.to_csv("reports/buy_and_hold.csv")
 
--- strategies/buy_and_hold.py
+++ strategies/buy_and_hold.py
@@ -1,5 +1,8 @@
 from zipline.api import order, symbol, record, set_benchmark
 from matplotlib import pyplot as plt
+from joblib import load
+import pandas as pd
+import numpy as np
 from datetime import datetime
 import pytz
 
@@ -8,18 +11,35 @@ import pandas as pd
 
 class BuyAndHold:
     stocks = ['AAPL']
+    lag = 33
+    forecast = 8
 
     def initialize(self, context):
         context.has_ordered = False
         context.stocks = self.stocks
         context.asset = symbol('AAPL')
         set_benchmark(False)
+        context.regressor = load('./strategies/models/rfr_regressor.joblib')
 
     def handle_data(self, context, data):
-        if not context.has_ordered:
-            for stock in context.stocks:
-                order(symbol(stock), 100)
-            context.has_ordered = True
+        for stock in context.stocks:
+            timeseries = data.history(
+                symbol(stock),
+                'price',
+                bar_count=self.lag,
+                frequency='1d'
+            )
+            np_timeseries = np.array(timeseries.values).reshape(1, -1)
+            preds = context.regressor.predict(np_timeseries)
+            max_price = np.max(preds)
+            historical_mean = np.mean(np_timeseries)
+
+        if max_price > historical_mean:
+            order(symbol(stock), 100)
+
+        if max_price < historical_mean:
+            order(symbol(stock), -100)
+
         record(AAPL=data.current(context.asset, 'price'))
 
     def _test_args(self):
@@ -41,4 +61,7 @@ class BuyAndHold:
         ax2.set_ylabel('price in EUR')
         plt.legend(loc=0)
         plt.show()
+
+        # returns, positions, transactions = pf.utils.extract_rets_pos_txn_from_zipline(perf)
+        # pf.create_returns_tear_sheet(returns, benchmark_ret=None)
 
